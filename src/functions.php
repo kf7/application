bb948894700ee94cc14e22361892b86489385d21
Anton <antonfedonjuk@gmail.com>
2020-07-08 16:07:21 +0300

    add cache, logs directories fix version in composer.json remove class Module

--- src/functions.php
+++ src/functions.php
@@ -7,20 +7,7 @@
  * @copyright 2020 Kohana team
  * @license BSD-3-Clause
  */
-
 namespace Kohana;
-
-/**
- * @return ModuleInterface
- */
-function app(): ModuleInterface
-{
-    static $instance;
-    if ($instance === null) {
-        $instance = new Application\Module();
-    }
-    return $instance;
-}
 
 /**
  * @param string $message Message text
